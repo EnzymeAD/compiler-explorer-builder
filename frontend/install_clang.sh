0f40ee0fc857277bbda0aa75048a96fc86f559f3
William Moses <gh@wsmoses.com>
2026-02-17 10:24:22 -0500

    franken ubuntu (#25)
    * franken ubuntu
    
    * Update base image from Ubuntu 22.04 to 24.04
    
    * Update base image to Ubuntu 24.04
    
    * try fix

--- frontend/install_clang.sh
+++ frontend/install_clang.sh
@@ -1,3 +1,4 @@
+curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 
 # Read versions from file available in Docker volume
 while read -r line || [ -n "$line" ]; do
@@ -5,10 +6,19 @@ while read -r line || [ -n "$line" ]; do
     # extract the major version number
     v=$(echo "$line" | cut -d- -f2 | cut -d. -f1)
 
-    curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
     if [ -n "$v" ]; then
-        echo "Installing clang-$v..."
-        apt-add-repository "deb http://apt.llvm.org/$(lsb_release -c | cut -f2)/ llvm-toolchain-$(lsb_release -c | cut -f2)-${v} main"
+        # Determine distro based on version
+        if [ "$v" -lt 17 ]; then
+            distro="jammy"
+        else
+            distro="noble"
+        fi
+
+        echo "Installing clang-$v using $distro..."
+
+        # Use the variable to construct the repository string
+        apt-add-repository "deb http://apt.llvm.org/${distro}/ llvm-toolchain-${distro}-${v} main"
+
         apt-get install -y llvm-${v}-dev libomp-${v}-dev lld-${v} clang-${v} libclang-${v}-dev
     fi
 done < /ver_data/clang-versions.txt
