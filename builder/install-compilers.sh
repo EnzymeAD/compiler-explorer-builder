7e1cfe02488410d08f4bab9a122ce21bdd727471
William Moses <gh@wsmoses.com>
2026-02-17 03:30:03 -0500

    Fix download (#20)
    * Fix download
    
    * cuda
    
    * fix
    
    * Use llvm apt repo
    
    * fixup
    
    * add missing
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * add key
    
    * add key
    
    * don't miss last line

--- builder/install-compilers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# /app/infra/bin/ce_install --enable nightly install "compilers/c++/mlir trunk"
-
-# /app/infra/bin/ce_install --enable nightly install "compilers/c++/nightly/clang assertions-trunk" "compilers/c++/mlir trunk"
---
+++ frontend/install_clang.sh
@@ -0,0 +1,14 @@
+
+# Read versions from file available in Docker volume
+while read -r line || [ -n "$line" ]; do
+    # format is like clang-15.0.0
+    # extract the major version number
+    v=$(echo "$line" | cut -d- -f2 | cut -d. -f1)
+
+    curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+    if [ -n "$v" ]; then
+        echo "Installing clang-$v..."
+        apt-add-repository "deb http://apt.llvm.org/$(lsb_release -c | cut -f2)/ llvm-toolchain-$(lsb_release -c | cut -f2)-${v} main"
+        apt-get install -y llvm-${v}-dev libomp-${v}-dev lld-${v} clang-${v} libclang-${v}-dev
+    fi
+done < /ver_data/clang-versions.txt
