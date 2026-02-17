8b494e7919a22718155bcacf7bbf6b853ea15071
William S. Moses <gh@wsmoses.com>
2026-02-17 02:19:17 -0500

    Use llvm apt repo

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -4,8 +4,6 @@ env >> /etc/environment
 
 mv -f /app/template/config/* /app/compiler-explorer/etc/config
 
-source /app/install-compilers.sh
-
 source /app/install-libraries.sh
 
 python3 /app/scripts/update-versions.py /app/data /app/template_files/etc/config /app/compiler-explorer/etc/config $(git -C /app/Enzyme rev-parse --short=7 HEAD) $(git -C /app/Enzyme-JaX rev-parse --short=7 HEAD)
