0282b81dfc9d646383f8edc440013b8f6d2c66da
William S. Moses <gh@wsmoses.com>
2026-02-17 12:57:20 -0500

    dont futz w config

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-env >> /etc/environment
-
-mv -f /app/template/config/* /app/compiler-explorer/etc/config
-
 source /app/install-libraries.sh
 
 python3 /app/scripts/update-versions.py /app/data /app/template_files/etc/config /app/compiler-explorer/etc/config $(git -C /app/Enzyme rev-parse --short=7 HEAD) $(git -C /app/Enzyme-JaX rev-parse --short=7 HEAD)
