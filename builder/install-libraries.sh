4b9ae7df325426eee45cc586671b3d865c9bd9f9
William S. Moses <gh@wsmoses.com>
2026-02-16 22:31:30 -0500

    Only use relevant julia versions

--- builder/install-libraries.sh
+++ builder/install-libraries.sh
@@ -3,4 +3,3 @@
 export JULIA_DEPOT_PATH="/opt/compiler-explorer/juliapackages"
 /opt/compiler-explorer/julia-1.10.*/bin/julia -e 'using Pkg; Pkg.add(["Enzyme", "Reactant"])'
 /opt/compiler-explorer/julia-1.11.*/bin/julia -e 'using Pkg; Pkg.add(["Enzyme", "Reactant"])'
-/opt/compiler-explorer/julia-1.12.*/bin/julia -e 'using Pkg; Pkg.add(["Enzyme", "Reactant"])'
